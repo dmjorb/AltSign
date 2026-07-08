a8b071b8790b112305cb1c97f38b1d0888283ccf
mahee96 <47920326+mahee96@users.noreply.github.com>
2026-07-08 18:57:27 +0530

    added timestamp to logs

--- SwiftBridge/AltSignLogging.swift
+++ SwiftBridge/AltSignLogging.swift
@@ -5,6 +5,7 @@
 //  Created by Magesh K on 28/06/26.
 //  Copyright © 2026 SideStore. All rights reserved.
 //
+import Foundation
 
 public enum AltSignLogging {
     public private(set) static var isLoggingEnabled = false
@@ -14,14 +15,23 @@ public enum AltSignLogging {
     }
 }
 
+private func getTag(level: String) -> String {
+    let formatter = DateFormatter()
+    formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSSSS'Z'"
+    formatter.timeZone = TimeZone(secondsFromGMT: 0)
+    formatter.locale = Locale(identifier: "en_US_POSIX")
+    let timestamp = formatter.string(from: Date())
+    return "\(timestamp) \(level): "
+}
+
 @inline(__always)
 public func debugLog(_ text: @autoclosure () -> String) {
-    print(text())
+    print("\(getTag(level: "DEBUG"))\(text())")
 }
 
 @inline(__always)
 public func verboseLog(_ text: @autoclosure () -> String) {
     if AltSignLogging.isLoggingEnabled {
-        print(text())
+        print("\(getTag(level: "TRACE"))\(text())")
     }
 }
