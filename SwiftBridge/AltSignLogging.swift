d45d4324096d6db7976c1ef81e81afc6c8a61b01
mahee96 <47920326+mahee96@users.noreply.github.com>
2026-07-11 02:11:10 +0530

    lets use short tag for logging levels

--- SwiftBridge/AltSignLogging.swift
+++ SwiftBridge/AltSignLogging.swift
@@ -29,7 +29,7 @@ public func debugLog(_ text: @autoclosure () -> String) {
     if !message.isEmpty && message.allSatisfy({ $0 == "\n" || $0 == "\r" }) {
         print(message, terminator: "")
     } else {
-        print("\(getTag(level: "DEBUG"))\(message)")
+        print("\(getTag(level: "[D]"))\(message)")
     }
 }
 
@@ -40,7 +40,7 @@ public func verboseLog(_ text: @autoclosure () -> String) {
         if !message.isEmpty && message.allSatisfy({ $0 == "\n" || $0 == "\r" }) {
             print(message, terminator: "")
         } else {
-            print("\(getTag(level: "TRACE"))\(message)")
+            print("\(getTag(level: "[V]"))\(message)")
         }
     }
 }
